500ca52f3a341d7fbff2d770670b559aea86bab1
Duc Thanh Tran <duc.thanh.tran.dtt@gmail.com>
2020-03-09 18:35:36 +0100

    Added Euclid's algorithms (algorithms E and F from volume 1, chapter 1.1)

---
+++ volume_1/chapter_1/euclid.cpp
@@ -0,0 +1,32 @@
+// Euclid's algorithm: algorithms E and F in "The Art of Programming, volume 1 (3rd edition)".
+#include <iostream>
+#include <string>
+
+
+// Algorithm E
+int euclid(const int m, const int n) {
+    const int r = m % n;
+    return r == 0 ? n : euclid(n, r);
+}
+
+
+// Algorithm F
+int euclid_f(int m, int n) {
+    m = m % n;
+    if(m == 0) { return n; }
+    n = n % m;
+    return n == 0 ? m : euclid_f(m, n);
+}
+
+
+// Arguments: s m n
+// If s == "F", use algorithm F (euclid_2), use algorithm E otherwise.
+int main(int argc, char* argv[]) {
+    const int m = std::stoi(argv[2]);
+    const int n = std::stoi(argv[3]);
+
+    const int gcd = std::string(argv[1]) == "F" ? euclid_f(m, n) : euclid(m, n);
+
+    std::cout << "Greatest common divisor of (" << m << ", " << n << ") is " << gcd << "\n";
+    return 0;
+}
